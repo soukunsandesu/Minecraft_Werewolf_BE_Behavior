4b8c9bc88007816c1b17dab93b0640814b3b070d
Nii0510-21 <nii20020510@gmail.com>
2023-03-06 15:38:18 +0900

    フォルダ構造変更

--- scripts/server/main.js
+++ scripts/server/main.js
@@ -1,11 +1,19 @@
 import { world } from "@minecraft/server";
 world.getDimension("overworld").runCommandAsync("scoreboard objectives add tick dummy")
+function CD(CD){world.getDimension("overworld").runCommandAsync(CD)}
 world.events.tick.subscribe(ev => {
     let tick = ev.currentTick
     world.getDimension("overworld").runCommandAsync("function ST-main")
     // if (tick % 5 == 0) { }
 })
 
-world.events.effectAdd.subscribe(ev=>{
+world.events.effectAdd.subscribe(ev => {
+   let entity = ev.entity
+   let effct = ev.effect.displayName
+    if (!entity.hasTag("PL")) {
+        CD("say test")
+        return
+    }
+    CD("say test2")
 
 })
